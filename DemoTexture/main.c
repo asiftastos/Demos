9c8f77f28c32327c8204d6867ce6b86507df1486
asiftastos <asiftastos@gmail.com>
2022-01-28 21:44:24 +0200

    Fixed demo texture

--- DemoTexture/main.c
+++ DemoTexture/main.c
@@ -1,8 +1,7 @@
-#include "demo.h"
-#include "demoui.h"
+#include "lgl.h"
+#include "GLFW/glfw3.h"
 
-static Demo* d = NULL;
-static Demoui* dui = NULL;
+static LGLContext* ctx = NULL;
 
 static GLuint quadVao;
 static GLuint quadVbo;
@@ -27,7 +26,6 @@ static void render2D()
 void init()
 {
     glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
-    dui = demouiInit(d);
 
     mat4s rotate = glms_rotate_y(GLMS_MAT4_IDENTITY, glm_rad(45.0f));
     mat4s trans = glms_translate_z(GLMS_MAT4_IDENTITY, 20.0f);
@@ -39,7 +37,8 @@ void init()
     //model = glms_mat4_mul(view, model);
     
     //glm_ortho(0.0f, d->fbSize[0], 0.0f, d->fbSize[1], 0.1f, 10.0f, proj);
-    proj = glms_perspective(glm_rad(60.0f), d->fbSize.x / d->fbSize.y, 0.1f, 1000.0f);
+    vec2s size = lglCurrentSize();
+    proj = glms_perspective(glm_rad(60.0f), size.x / size.y, 0.1f, 1000.0f);
     proj = glms_mat4_mul(proj, view);
 
     quadTexture = textureCreate((TextureParams){
@@ -49,10 +48,10 @@ void init()
         .minFilter = GL_LINEAR,
         .magFilter = GL_LINEAR,
         .mipmaps = false,
-        .path = "assets/textures/wall.jpg"
+        .path = "wall.jpg"
     });
 
-    textureShader = shaderCreate("assets/shaders/demotext.vert", "assets/shaders/texture.frag");
+    textureShader = shaderCreate("demotext.vert", "texture.frag");
     shaderUse(textureShader);
     shaderSetupUniforms(textureShader, 2, (const char*[]){"model", "proj"});
     glUseProgram(0);
@@ -84,62 +83,45 @@ void terminate()
 {
     textureDestroy(quadTexture);
 
-    demouiTerminate();
     printf("Demo texturing terminated\n");
 }
 
 void update()
 {
-    if(d->keys[GLFW_KEY_ESCAPE].pressed)
-        glfwSetWindowShouldClose(d->window, GLFW_TRUE);
-    
-    if(d->keys[GLFW_KEY_F3].pressed)
-        demouiToggleShowGraphs();
-    
-    if(d->keys[GLFW_KEY_F4].pressed)
-        demouiToggleGraph();
-}
-
-void render()
-{
-    switch (d->renderPass)
-    {
-    case PASS_3D:
-        demouiStartGPUTimer();
-        render2D();
-        break;
-    case PASS_2D:
-        break;
-    case PASS_UI:
-        {
-            demouiBeginRender(d->winSize.x, d->winSize.y, d->winSize.x / d->fbSize.x);
-        }
-        break;
-    case PASS_FLUSH:
-        {
-            demouiEndRender(d->winSize.x - 200 - 5, 5);
-            demouiUpdateGraphs(d->cpuTime, d->frameDelta);
-        }
-        break;
-    default:
-        break;
-    }
+    if(lglIsKeyPressed(GLFW_KEY_ESCAPE))
+        lglClose();
 }
 
 int main(void)
 {
-    DemoParams dmParams = {
-        .windowSize = (vec2s){{1280, 960}},
-        .title = "Demo 2D",
-        .fInit = init,
-        .fTerminate = terminate,
-        .fUpdate = update,
-        .fRender = render,
-        .fullscreen = false
-    };
-    d = demoCreate(dmParams);
-    demoRun();
-    demoDestroy();
+    ctx = lglCreateContext((LGLParams) {
+            .title="Demo Texture",
+            .fullscreen=false,
+            .windowSize=(vec2s){{1280.0f, 960.0f}}
+    });
+    
+    init();
+
+    vec4s clearColor = (vec4s){ {0.0f, 0.0f, 0.0f, 1.0f} };
+    float depth = 1.0f;
+
+    while (lglIsRunning())
+    {
+        lglUpdateInput();
+
+        update();
+
+        glClearBufferfv(GL_COLOR, 0, clearColor.raw);
+        glClearBufferfv(GL_DEPTH, 0, &depth);
+
+        render2D();
+
+        lglSwapAndPoll();
+    }
+
+    terminate();
+
+    lglDestroyContext(ctx);
 
     return 0;
 }
